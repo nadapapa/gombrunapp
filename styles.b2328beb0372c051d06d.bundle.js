eada4e7531d6d46da8ea3dd0cb68782ce18806c4
Travis CI <nadapapa@gmail.com>
2016-12-13 18:19:08 +0000

    Deploy to GitHub Pages: c86ab6c864f55c1963d9037f4fa06fbced709ac6

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{323:function(n,o){},606:function(n,o,c){n.exports=c(323)}},[606]);
+webpackJsonp([1,3],{334:function(n,o){},625:function(n,o,c){n.exports=c(334)}},[625]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
