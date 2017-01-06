c02337ed301e9e89b79add82be127ef17b74b02e
nadapapa <nadapapa@gmail.com>
2017-01-06 20:28:36 +0100

    refactor text component

--- nativescript/app/main.ts
+++ nativescript/app/main.ts
@@ -1,6 +1,13 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
 import { NgModule } from "@angular/core";
-import { AppModule } from "./app.module";
+import { AppComponent } from "./app/app.component";
 
-platformNativeScriptDynamic({startPageActionBarHidden: true}).bootstrapModule(AppModule);
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+   imports: [NativeScriptModule],
+})
+class AppComponentModule {}
+
+platformNativeScriptDynamic().bootstrapModule(AppComponentModule);
--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -9,6 +9,7 @@ import { OnInit, Input } from '@angular/core';
 })
 export class TextComponent implements OnInit {
   @Input() text: string;
+  private fontSize: string = "5vw";
 
   constructor() { }
 
